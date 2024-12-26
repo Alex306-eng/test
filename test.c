520f5826065e739d62f24cc414cacc64138f3c3a
AlexK <kovalenkoalex20@gmail.com>
2024-12-26 21:51:48 +0700

    Solved problem with condition of place

--- test.c
+++ test.c
@@ -2,24 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <time.h>
+#include <string.h>
 
-#define SIZE 10
-#define MINES 12
-
-
+#define SIZE 5
+#define MINES 6
 
 int main(void)
 {
-    srand(time(NULL));
-
     char pg[SIZE][SIZE] = {'0'};
-    char coors[2] = {0};
-  
+    memset(pg, '0', sizeof pg);
 
-    for (size_t i = 0; i < MINES; i++)
+    char coors[2] = {0};
+
+    for (size_t _ = 0; _ < MINES; _++)
     {
         bool flag = true;
-        
+
         do
         {
             flag = true;
@@ -27,26 +25,24 @@ int main(void)
             coors[0] = rand() % SIZE;
             coors[1] = rand() % SIZE;
 
-            for (size_t i = -1; i < 2; i++)
+            for (int i = -1; i != 2; i++)
             {
-                if (0 < coors[0] + i && coors[0] + i < 10)
-                    
-                    for (size_t k = -1; k < 2; k++)
+                if ((-1 < coors[0] + i) && (coors[0] + i < 10))
+
+                    for (int k = -1; k != 2; k++)
                     {
-                        if (0 < coors[1] + k && coors[1] + k < 10)
+                        if ((-1 < coors[1] + k) && (coors[1] + k < 10))
                             if (pg[coors[0] + i][coors[1] + k] == '*')
                                 flag = false;
                     }
-                
+
                 if (flag == false)
                     break;
             }
 
         } while (!flag);
 
-        
         pg[coors[0]][coors[1]] = '*';
-
     }
 
     for (size_t i = 0; i < SIZE; i++)
@@ -57,9 +53,7 @@ int main(void)
         }
 
         printf("\n");
-        
     }
-    
 
     //__assert_verify_pg(pg); // для тестирования (не убирать и должна идти непосредственно перед return 0)
     return 0;
